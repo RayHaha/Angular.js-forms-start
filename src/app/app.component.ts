09c9138ba14665923c89cd06ec32de8425a9df11
RayHaha <g820324@gmail.com>
2020-03-19 00:24:32 -0400

    set default values with ngModel Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f', {static: false}) singupForm: NgForm;
-
+  defaultQuestion = 'pet';
   suggestUserName() {
     const suggestedName = 'Superuser';
   }

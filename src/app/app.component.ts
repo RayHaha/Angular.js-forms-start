b3dc25dddb3d1868fa73856dff91813e931136a4
RayHaha <g820324@gmail.com>
2020-03-22 15:31:57 -0400

    handle radio buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   @ViewChild('f', {static: false}) singupForm: NgForm;
   defaultQuestion = 'pet';
   answer = '';
+  genders = ['male','female'];
   suggestUserName() {
     const suggestedName = 'Superuser';
   }

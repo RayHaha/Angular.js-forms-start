12a6834258d60dc548d05826fc3a329ebc4c8770
RayHaha <g820324@gmail.com>
2020-03-23 21:50:05 -0400

    set and patch form values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,31 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f', {static: false}) singupForm: NgForm;
+  @ViewChild('f', {static: false}) signupForm: NgForm;
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male','female'];
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    // method 1 set the whole form
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+
+    // method 2 patch a part of form
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+
   }
 
   // onSubmit(form: NgForm){
@@ -20,6 +39,6 @@ export class AppComponent {
   // }
 
   onSubmit(){
-    console.log(this.singupForm);
+    console.log(this.signupForm);
   }
 }
